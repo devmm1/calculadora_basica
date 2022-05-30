c626a52fefb219d81adcc271038faf986a8d8b1a
maykerdev <mayker.montoya01@gmail.com>
2022-05-29 21:46:16 -0400

    mi primer commit

--- procesar.php
+++ procesar.php
@@ -3,4 +3,5 @@
     $cal = new calculator();
 
     echo $cal->calculate($_POST['value1'],$_POST['value2'],$_POST['option']);
+    echo $cal->calculate($_POST['value1'],$_POST['value2'],$_POST['option']);
  ?>
