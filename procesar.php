7002abeb8226bd559c25ab2dcf58a9b0f5148f08
maykerdev <mayker.montoya01@gmail.com>
2022-05-29 22:03:24 -0400

    quinta modificacion

--- procesar.php
+++ procesar.php
@@ -3,5 +3,4 @@
     $cal = new calculator();
 
     echo $cal->calculate($_POST['value1'],$_POST['value2'],$_POST['option']);
-    echo $cal->calculate($_POST['value1'],$_POST['value2'],$_POST['option']);
  ?>
